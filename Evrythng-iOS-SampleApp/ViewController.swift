2c8fbbcc68629d305b376e1bc3f8f582becb3842
JD Castro <jd@imfreemobile.com>
2017-05-09 15:54:08 +0800

    Gitignore Updated ViewController and Main.storyboard

--- Evrythng-iOS-SampleApp/ViewController.swift
+++ Evrythng-iOS-SampleApp/ViewController.swift
@@ -11,10 +11,26 @@ import EvrythngiOS
 
 class ViewController: UIViewController {
 
+    // MARK: - IBActions
+    
+    @IBAction func actionScan(_ sender: UIButton) {
+        let evrythngScanner = EvrythngScanner.init(presentingVC: self)
+        //evrythngScanner.scanBarcode()
+        let result = evrythngScanner.queryScanResult(barcode: "own_vc_1234567")
+        print("Query Scan Result: \(result.result)")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         Evrythng.initialize(delegate: self)
+        
+        /*
+        let provider = Ev<MyService>()
+        provider.request(.createUser(firstName: "James", lastName: "Potter")) { result in
+            // do something with the result (read on for more details)
+        }
+         */
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Evrythng-iOS-SampleApp' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Evrythng-iOS-SampleApp
+  pod 'EvrythngiOS', :git => 'https://github.com/imfree-jdcastro/Evrythng-iOS-SDK.git', :tag => '0.0.192'
+
+  target 'Evrythng-iOS-SampleAppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Evrythng-iOS-SampleAppUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
