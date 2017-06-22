b64fa1ae2877567eec235b5687afb414eb77a019
JD Castro <jd@imfreemobile.com>
2017-06-22 08:15:48 +0800

    *Updated sample harness app. *Enabled debugging *Moved showing of ProgressHUD in ViewController from EvrythngScanner

--- Evrythng-iOS-SampleApp/AppDelegate.swift
+++ Evrythng-iOS-SampleApp/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         Evrythng.initialize(delegate: nil)
-        
+        Evrythng.DEBUGGING_ENABLED = true
         return true
     }
 
